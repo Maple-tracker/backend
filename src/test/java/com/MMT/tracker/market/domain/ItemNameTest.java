fc6cb531001ce2f7ca8d6c106244029dd87af9a6
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-04-02 00:54:33 +0900

    test(ItemNameTest): Enum 도메인 클래스 테스트 추가

---
+++ src/test/java/com/MMT/tracker/market/domain/ItemNameTest.java
@@ -0,0 +1,60 @@
+package com.MMT.tracker.market.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.mmt.tracker.advice.BadRequestException;
+import com.mmt.tracker.market.domain.ItemName;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("ItemName Enum 테스트")
+class ItemNameTest {
+
+    @Test
+    @DisplayName("유효한 아이템 이름으로 fromString 테스트")
+    void testFromString_ValidExact() {
+        // Given
+        String validName = "샤이니 레드 워리어 마이스터 심볼";
+
+        // When
+        ItemName itemName = ItemName.fromString(validName);
+
+        // Then
+        assertThat(itemName).isEqualTo(ItemName.EYE_ECC1);
+    }
+
+    @Test
+    @DisplayName("Enum 스타일 아이템 이름으로 fromString 테스트")
+    void testFromString_EnumStyle() {
+        // Given
+        String enumStyleName = "EYE_ECC1";
+
+        // When
+        ItemName itemName = ItemName.fromString(enumStyleName);
+
+        // Then
+        assertThat(itemName).isEqualTo(ItemName.EYE_ECC1);
+    }
+
+    @Test
+    @DisplayName("잘못된 아이템 이름으로 fromString 실패 테스트")
+    void testFromString_Invalid() {
+        // Given
+        String invalidName = "존재하지 않는 아이템";
+
+        // When & Then
+        assertThatThrownBy(() -> ItemName.fromString(invalidName))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Unknown item name");
+    }
+
+    @Test
+    @DisplayName("null 아이템 이름으로 fromString 실패 테스트")
+    void testFromString_Null() {
+        // When & Then
+        assertThatThrownBy(() -> ItemName.fromString(null))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Item name cannot be null");
+    }
+}
