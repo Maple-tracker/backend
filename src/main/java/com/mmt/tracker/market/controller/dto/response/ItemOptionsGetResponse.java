e3496ee2df5a9fa488598ced50f2470880773190
YujinJuhn <holsya@naver.com>
2025-04-08 18:43:33 +0900

    feat: dto 추가

---
+++ src/main/java/com/mmt/tracker/market/controller/dto/response/ItemOptionsGetResponse.java
@@ -0,0 +1,7 @@
+package com.mmt.tracker.market.controller.dto.response;
+
+import java.util.List;
+
+public class ItemOptionsGetResponse {
+    private List<AvailableItemOption> availableItemOptions;
+}
