9013da2682ee5398a21714e73ce49cd416a2f8e6
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-05-22 17:15:55 +0900

    refactor: DTO field명 수정

--- src/main/java/com/mmt/tracker/market/controller/dto/response/CategoryOption.java
+++ src/main/java/com/mmt/tracker/market/controller/dto/response/CategoryOption.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public record CategoryOption(
         String name,
         Map<String, CategoryOption> subCategories,
-        Long optionIds
+        Long optionId
 ) {
 
 }
