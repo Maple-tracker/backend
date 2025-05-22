979146c5d25348719be0a3f5a1f9afff223d07f0
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-05-22 16:59:59 +0900

    fix: 노작 여부 필터링 로직 수정

--- src/main/java/com/mmt/tracker/market/controller/dto/response/CategoryOption.java
+++ src/main/java/com/mmt/tracker/market/controller/dto/response/CategoryOption.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public record CategoryOption(
         String name,
         Map<String, CategoryOption> subCategories,
-        List<Long> optionIds
+        Long optionIds
 ) {
 
 }
