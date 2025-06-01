70e2dcd560ea005c09198e351b4ffc3af0cf94ed
YujinJuhn <holsya@naver.com>
2025-06-01 16:34:34 +0900

    feat: fromString 추가

--- src/main/java/com/mmt/tracker/market/domain/ItemSlot.java
+++ src/main/java/com/mmt/tracker/market/domain/ItemSlot.java
@@ -1,5 +1,7 @@
 package com.mmt.tracker.market.domain;
 
+import com.mmt.tracker.advice.BadRequestException;
+
 public enum ItemSlot {
     EYE_ECC("눈장식"),
     FACE_ECC("얼굴장식"),
@@ -16,5 +18,25 @@ public enum ItemSlot {
 
     public String getValue() {
         return value;
+    }
+
+    public static ItemSlot fromItemName(ItemName itemName) {
+        String enumName = itemName.name();
+
+        if (enumName.startsWith("EYE_ECC")) {
+            return ItemSlot.EYE_ECC;
+        } else if (enumName.startsWith("FACE_ECC")) {
+            return ItemSlot.FACE_ECC;
+        } else if (enumName.startsWith("PENDANT")) {
+            return ItemSlot.PENDANT;
+        } else if (enumName.startsWith("BELT")) {
+            return ItemSlot.BELT;
+        } else if (enumName.startsWith("RING")) {
+            return ItemSlot.RING;
+        } else if (enumName.startsWith("EARRING")) {
+            return ItemSlot.EARRINGS;
+        } else {
+            throw new BadRequestException("존재하지 않는 슬롯: " + itemName.getValue());
+        }
     }
 }
